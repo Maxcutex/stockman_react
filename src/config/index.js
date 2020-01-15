78e1ee4348b8fc05ff46be0ade5431c36bcd9200
Eno Bassey <ennyboy@gmail.com>
2020-01-15 23:41:11 +0100

    update api url

--- src/config/index.js
+++ src/config/index.js
@@ -2,22 +2,22 @@ const allConfig = {
     development: {
         BASE_URL: 'http://localhost:3000',
         STOCKMAN_API_BASE_URL_AWS: 'http://stockmanapi-env.fptfzc4bbf.us-east-2.elasticbeanstalk.com/api/v1',
-        STOCKMAN_API_BASE_URL: 'http://stockman-api.herokuapp.com/api/v1',
+        STOCKMAN_API_BASE_URL: 'https://stockman-api.herokuapp.com/api/v1',
     },
 
     production: {
         BASE_URL: 'https://stockman-app.herokuapp.com',
-        STOCKMAN_API_BASE_URL: 'http://stockman-api.herokuapp.com/api/v1',
+        STOCKMAN_API_BASE_URL: 'https://stockman-api.herokuapp.com/api/v1',
     },
 
     staging: {
         BASE_URL: 'https://staging-stockman-app.herokuapp.com',
-        STOCKMAN_API_BASE_URL: 'http://stockman-api.herokuapp.com/api/v1',
+        STOCKMAN_API_BASE_URL: 'https://stockman-api.herokuapp.com/api/v1',
     },
 
     test: {
         BASE_URL: 'http://localhost:3000',
-        STOCKMAN_API_BASE_URL: 'http://stockman-api.herokuapp.com/api/v1',
+        STOCKMAN_API_BASE_URL: 'https://stockman-api.herokuapp.com/api/v1',
     },
 };
 
