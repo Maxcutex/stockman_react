30bbdec7d4a64e61a5d1f54804ab920a3d74c5b2
Eno Bassey <ennyboy@gmail.com>
2020-08-24 12:31:43 +0100

    rework button alignment

--- src/components/search-price/search-price.js
+++ src/components/search-price/search-price.js
@@ -15,18 +15,19 @@ class SearchPrice extends React.Component {
                 <form onSubmit={this.props.handleSubmit}>
                     <div className='col-sm-3'>Search Price List By Date</div>
 
-                    <div className='col-sm-3'>Date:  
-                 
-                        <DatePicker
+                    <div className='col-sm-8'>
+                        <div style={{float:'left', marginRight: '5px'}}>Date:</div>  
+                        <div style={{float:'left', marginRight: '5px'}}><DatePicker
                             selected={this.props.date}
                             onChange={this.props.handleChange}
                             
                             dateFormat="yyyy/MM/dd"
-                            />
+                            /></div>
+                        <div style={{float:'left', marginRight: '5px;'}}><button type="submit" value="Submit">Get Price List </button></div>
                         
 
                     </div>
-                    <div className='col-sm-3'><CustomButton type="submit" value="Submit">Get Price List </CustomButton></div>
+                    <div className='col-sm-3'></div>
                 </form>
             </div>
            
